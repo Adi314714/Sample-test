6063a2a140f95bcb3fca36c3d3de1fac3b24aa7c
Adi314714 <adiazar10@gmail.com>
2025-02-06 10:25:48 +0200

    finish

--- airplane/airplane.js
+++ airplane/airplane.js
@@ -1,7 +1,7 @@
 document.getElementById("start-fly").addEventListener("click", function () {
   const airplaneImg = document.querySelector('img[src="./airplane.png"]');
-  airplaneImg.style.bottom = "90%";
-  airplaneImg.style.left = "90%";
+  airplaneImg.style.bottom = "70%";
+  airplaneImg.style.left = "30%";
 });
 document.getElementById("reset").addEventListener("click", function () {
   const airplaneImg = document.querySelector('img[src="./airplane.png"]');
